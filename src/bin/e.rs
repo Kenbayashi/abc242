904ddf9d026dcc86d3b96f00aaa485c2c23df046
Kenbayashi <kenbayashi.gaming.00@gmail.com>
2022-03-11 14:00:54 +0900

    e

--- src/bin/e.rs
+++ src/bin/e.rs
@@ -1,3 +1,32 @@
+use proconio::input;
+use proconio::marker::Bytes;
+
 fn main() {
-    unimplemented!();
+    input! {
+        t: usize,
+        tuples: [(usize, Bytes); t],
+    }
+
+    let offset = 'A' as u8;
+
+    for (n, s) in tuples.into_iter() {  
+        let s = s.into_iter().map(|c| c - offset).collect::<Vec<u8>>();
+
+        let count = |n: usize, s: &Vec<u8>| {
+            (0..=(n - 1) / 2).into_iter()
+                            .fold(0, |acc, num| (acc * 26 + (s[num] as u64)) % 998244353)
+        };
+
+        let issue = |n: usize, s: &Vec<u8>| {
+            let mut s_copy = s.clone();
+            (0..=(n - 1) / 2).into_iter()
+                             .for_each(|num| s_copy[n - num - 1] = s_copy[num]);
+
+            s_copy <= *s
+        };
+
+        let ans = count(n, &s) + if issue(n, &s) {1} else {0};
+
+        println!("{}", ans);
+    }
 }
